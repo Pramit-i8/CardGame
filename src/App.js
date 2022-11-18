55be34416509cf3dc3096bc1ff5c7c8eb9953171
Pramit-i8 <pramitdey22@gmail.com>
2022-11-17 22:10:35 -0500

    style changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import SingleCard from './components/SingleCard';
 
 // array of card images
+//  3 by 4 grid: 12 cards, 6 pairs
 const cardImages = [
   { "src": "/images/green2.png", matched: false },
   { "src": "/images/blue4.png", matched: false },
@@ -14,78 +15,80 @@ const cardImages = [
 ]
 
 function App() {
-  const [cards, setCards] = useState([])
-  const[turns, setTurns] = useState(0)
+
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
   const [choiceOne, setChoiceOne] = useState(null);
   const [choiceTwo, setChoiceTwo] = useState(null);
   const [disabled, setDisabled] = useState(false);
 
-  
-  //cards shuffled 
+  // shuffle cards algorithm
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]      
-    //array shuffle  
-    .sort(() => Math.random() - 0.5)                        
-    .map((card) => ({ ...card, id: Math.random() })) 
-    
-    setCards(shuffledCards)
-    setTurns(0)
+      .sort(() => Math.random() - 0.5)                        
+      .map((card) => ({ ...card, id: Math.random() }))        
+
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setCards(shuffledCards);
+    setTurns(0);
   }
 
-  //handling choices
+  //user choice, update choices 
   const handleChoice = (card) => {
-    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)  
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)        
   }
-  // compare two selected cards
+
+  // restart game to new grid 
   useEffect(() => {
-    if (choiceOne && choiceTwo){
-      setDisabled(true)
-      
+    shuffleCards()
+  }, [])
+
+  // compare between the two selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true);
       if (choiceOne.src === choiceTwo.src) {
         setCards(prevCards => {
-          return prevCards.map(card => {
-            if (card.src === choiceOne.src){
-              return {...card, matched: true}
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true }
+            } else {
+              return card;
             }
-            else
-            {
-              return card
-            }
-          
           })
         })
-        resetTurn()
-      }
-      else
-      {
-        setTimeout(() => resetTurn(),1000)
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1000);
       }
     }
-  },[choiceOne, choiceTwo])
-  
+  }, [choiceOne, choiceTwo])
+
   // reset choices and increase number of turns
   const resetTurn = () => {
     setChoiceOne(null);
     setChoiceTwo(null);
     setTurns(prevTurns => prevTurns + 1);
-    setDisabled(false)
+    setDisabled(false);
   }
 
   return (
     <div className="App">
       <h1>Memory Game</h1>
+      <h5>Select two cards of same value</h5>
       <button onClick={shuffleCards}>New Game</button>
-
-      <div className='card-grid'>
-        {cards.map(card => (
-          <SingleCard 
-          key={card.id} card={card} 
-          handleChoice={handleChoice} 
-          fliped={card === choiceOne || card === choiceTwo || card.matched}
-          disabled={disabled}
+      <p>Moves: {turns}</p>
+      <div className="card-grid">
+        {cards.map((card) => (
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            cardFlipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
           />
-           
-        ))}  
+        ))}
       </div>
     </div>
   );
